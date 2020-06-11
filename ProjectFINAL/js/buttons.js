43b345fb01744063e870d2965f7750815dbb44ea
meganmunce <meganmunce2022@u.northwestern.edu>
2020-06-10 17:23:11 -0700

    project final

---
+++ ProjectFINAL/js/buttons.js
@@ -0,0 +1,246 @@
+
+
+multimediaArticleOne = `
+<section class="article1 articlehover">
+  <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/824934847&color=ff5500"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;
+  font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;" class="image">
+    <a href="https://soundcloud.com/the-daily-northwestern" title="The Daily Northwestern" target="_blank" style="color: #cccccc; text-decoration: none;">The Daily Northwestern</a> · <a href="https://soundcloud.com/the-daily-northwestern/nu-declassified-path-to-the-pulitzer" title="NU Declassified: Path to the Pulitzer" target="_blank" style="color: #cccccc; text-decoration: none;">NU Declassified: Path to the Pulitzer</a></div>
+  <h3>NU Declassified: Path to the Pulitzer</h3>
+  <p>Evan Hill and Brian Rosenthal have a lot in common: They both graduated from Medill, were editors at The Daily Northwestern, are now New York Times investigative journalists — and recently won the highest honor in journalism: a Pulitzer Prize.</p>
+  <br>
+  <a href="https://dailynorthwestern.com/2020/05/21/multimedia/nu-declassified-path-to-the-pulitzer/" target="_blank">Read More</a>
+</section>
+`
+
+multimediaArticleTwo = `
+<section class="article2 articlehover">
+<p class="codepen" data-height="471" data-theme-id="light" data-default-tab="result" data-user="meganmunce" data-slug-hash="PLRqgN" style="height: 471px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="Jour 201-2 Final Project">
+  <span>See the Pen <a href="https://codepen.io/meganmunce/pen/PLRqgN">
+  Jour 201-2 Final Project</a> by Megan Munce (<a href="https://codepen.io/meganmunce">@meganmunce</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+</section>`
+
+resumeContent = `
+
+<section class="d shown">
+<h2>Work Experience</h2>
+<h3>The Daily Northwestern</h3>
+<p>Assistant Campus Editor</p>
+<p>September 2018 - current</p>
+<p>Evanston, IL/Remote</p>
+  <ul>
+    <li>Assist the campus editor in editing all campus desk articles for style and clarity</li>
+    <li>Pitch and budget weekly stories</li>
+    <li>Cover student activists at Northwestern</li>
+  </ul>
+  <h3>Parity.org</h3>
+  <p>Content Volunteer</p>
+  <p>August 2019 - January 2020</p>
+  <p>Remote</p>
+    <ul>
+      <li>Research current events and news about women in business leadership</li>
+      <li>Create unique content for Twitter, Facebook, and LinkedIn to promote content and the nonprofit's message</li>
+      <li>Analyze past analytics to grow engagement across platforms</p>
+    </ul>
+    <h3>The Organization for World Peace</h3>
+    <p>Junior Correspondent</p>
+    <p>April 2019 - August 2020</p>
+    <p>Remote</p>
+      <ul>
+        <li>Research current events and news about developments in international conflict</li>
+        <li>Write weekly articles briefing about the development, its past context, and </li>
+      </ul>
+</section>
+<section class="e shown">
+<h2>Education</h2>
+<h3> Presentation High School </h3>
+<p>San Jose, CA</p>
+<p><b>Degree: </b>High School Diploma</p>
+  <p><b>GPA: </b>4.43</b></p>
+  <p><b>Activities: </b></p>
+  <ul>
+    <li>Vice President of Speech & Debate</li>
+    <li>Student Council Class Officer</li>
+    <li> Students for Political Action Officer</li>
+    <li>Jazz Band co-founder</li>
+    <li>Big Sister</li>
+    <li>Junior Varsity Track & Field</li>
+  </ul>
+  <h3> Northwestern University </h3>
+  <p>Evanston, IL</p>
+  <p><b>Degree: </b>Bachelor's of science in journalism, second major in political science (expected June 2022)</p>
+    <p><b>GPA: </b>3.85</b></p>
+    <p><b>Activities: </b></p>
+    <ul>
+      <li>The Daily Northwestern</li>
+      <li>Northwestern Mock Trial</li>
+      <li>Medill News Service</li>
+      <li>Peer Adviser</li>
+      <li>Womxn in Law</li>
+      <li>Delta Gamma women's fraternity</li>
+    </ul>
+</section>
+<section class="f shown">
+<h2>Skills</h2>
+<p>Languages</p>
+<ul>
+<li>Native fluency in English</li>
+<li>Professional fluency in Spanish (speaking, reading, writing)</li>
+</ul>
+<p>Computer Science</p>
+<ul>
+<li>Novice HTML, CSS, Javascript</li>
+<li>Novice Python</li>
+</ul>
+<p>Creativity & Productivity Tools</p>
+<ul>
+<li>Advanced in Google Suite</li>
+<li>Advanced in Microsoft Suite</li>
+</ul>
+<p>Journalism</p>
+<ul>
+<li>Advanced in AP style</li>
+<li>Proficient in Adobe Audition</li>
+</ul>
+<p>Design & Visualization</p>
+<ul>
+<li>Proficient in Adobe Illustrator</li>
+<li>Proficient in Adobe InDesign</li>
+<li>Proficient in Adobe Photoshop</li>
+<li>Proficient in Adobe After Effects</li>
+<li>Proficient in Procreate</li>
+<li>Proficient in Tableau</li>
+</ul>
+</section>
+<section class="g shown">
+<p>empty for now!</p>
+</section>
+`
+
+resumeHidden = `
+<section class="d hidden">
+</section>
+<section class="e hidden">
+</section>
+<section class="f hidden">
+</section>
+<section class="g hidden">
+</section>`
+
+topArticles = `
+<section class="d shown">
+<a href="https://dailynorthwestern.com/2020/05/25/campus/desi-american-women-fight-period-stigma-during-asian-pacific-american-heritage-month/" target="_blank">
+<section class="article1 articlehover">
+<img src="https://dailynorthwestern.com/wp-content/uploads/2020/05/FTP_-02_5.png" class="image">
+<h3>Desi-American women fight period stigma during Asian Pacific American Heritage Month</h3>
+<p>Meghna Gaddam, Samanvi Kanugula and Mahie Gopalka are the three Desi-American women behind PERIOD at Northwestern fighting for an end to period stigma in their culture and the state of Illinois.</p>
+<br>
+<p>Click to read more.
+</section>
+</a>
+</section>
+<section class="e shown">
+<a href="https://dailynorthwestern.com/2020/04/10/campus/supreme-court-decision-disrupts-students-voting-plan/" target="_blank">
+<section class="article2 articlehover">
+  <img src="https://dailynorthwestern.com/wp-content/uploads/2020/04/US-NEWS-CORONAVIRUS-LIVE-UPDATES-STIMULUS-CHECKS-AU-900x695.jpg" class="image">
+  <h3>Supreme Court decision disrupts students’ voting plan</h3>
+  <p>The Supreme Court of the United States reversed a lower court’s ruling allowing Wisconsin to extend the deadline for absentee ballots to be counted on Tuesday, forcing many of the state’s residents to choose between voting and potential exposure to COVID-19.</p>
+  <br>
+  <p>Click to read more.</p>
+</section>
+</a>
+</section>
+<section class="f shown">
+<a href="https://dailynorthwestern.com/2018/10/12/campus/dolores-huerta-speaks-at-northwestern-for-womens-center-keynote-address/" target="_blank">
+<section class="article4 articlehover">
+  <img src="https://dailynorthwestern.com/wp-content/uploads/2018/10/HUERTA1-ColinBoyle-WEB-900x600.jpg" class="image">
+  <h3>Dolores Huerta hits history, politics, education</h3>
+  <p>The Northwestern University Women’s Center kicked off its year-long theme of “Gender, Work, & Power” with a keynote address by civil rights activist Dolores Huerta on Thursday.</p>
+  <br>
+  <p>Click to read more.</p>
+</section>
+</a>
+</section>
+<section class="g shown">
+<a href="https://dailynorthwestern.com/2019/04/16/campus/northwestern-joins-action-collaborative-to-address-sexual-and-gender-harassment-in-higher-education/" target="_blank">
+<section class="article3 articlehover">
+  <img src="https://dailynorthwestern.com/wp-content/uploads/2019/04/HARASSMENT_DanielTian_file_WEB-900x598.jpg" class="image">
+  <h3>Northwestern joins action collaborative to address sexual and gender harassment in higher education</h3>
+  <p>Northwestern recently announced its commitment to address that problem, as well as other forms of gender-based discrimination, by joining the Action Collaborative on Preventing Sexual Harassment in Higher Education as a founding member.</p>
+  <br>
+  <p>Click to read more.</p>
+</section>
+</a>
+</section>
+`
+
+// BUTTON FUNCTIONS
+
+resumeShown = "false"
+topShown = "false"
+multimediaShown = "false"
+
+const checkResume = () => {
+    if (resumeShown == "false" && topShown == "false") {
+      document.querySelector('.viewer').innerHTML = resumeContent;
+      document.querySelector('.showResume').innerHTML= "Hide Resume";
+      resumeShown = "true";
+      topShown = "false";
+      return resumeShown;
+      return topShown;
+    }
+    else if (resumeShown == "false" && topShown == "true") {
+      document.querySelector('.viewer').innerHTML = resumeHidden;
+      document.querySelector('.viewer').innerHTML = resumeContent;
+      document.querySelector('.showResume').innerHTML= "Hide Resume";
+      document.querySelector('.topArticles').innerHTML= "Show Print Articles";
+      resumeShown = "true";
+      topShown = "false";
+      return resumeShown;
+      return topShown;
+    }
+    else if (resumeShown == "true" && topShown == "false") {
+      document.querySelector('.viewer').innerHTML = resumeHidden;
+      document.querySelector('.showResume').innerHTML= "Show Resume";
+      document.querySelector('.topArticles').innerHTML= "Show Print Articles";
+      resumeShown = "false";
+      topShown = "false"
+      return resumeShown;
+      return topShown;
+    }
+};
+
+const checkTop = () => {
+    if (resumeShown == "false" && topShown == "false") {
+      document.querySelector('.viewer').innerHTML = topArticles;
+      document.querySelector('.topArticles').innerHTML= "Hide Print Articles";
+      resumeShown = "false";
+      topShown = "true";
+      return resumeShown;
+      return topShown;
+    }
+    else if (resumeShown == "false" && topShown == "true") {
+      document.querySelector('.viewer').innerHTML = resumeHidden;
+      document.querySelector('.topArticles').innerHTML= "Show Print Articles";
+      resumeShown = "false";
+      topShown = "false";
+      return resumeShown;
+      return topShown;
+    }
+    else if (resumeShown == "true" && topShown == "false") {
+      document.querySelector('.viewer').innerHTML = resumeHidden;
+      document.querySelector('.viewer').innerHTML = topArticles;
+      document.querySelector('.topArticles').innerHTML= "Hide Print Articles";
+      document.querySelector('.showResume').innerHTML= "Show Resume";
+      resumeShown = "false";
+      topShown = "true"
+      return resumeShown;
+      return topShown;
+    }
+};
+
+document.querySelector('.showResume').onclick = checkResume;
+document.querySelector('.topArticles').onclick = checkTop;
+// document.querySelector('.multimediaArticles').onclick = checkMultimedia;
